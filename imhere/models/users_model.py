e4f4cbbad94bec78cdfcc672794d6d5b4f7a531d
larryzhao <larrylzhao@gmail.com>
2017-04-30 22:42:38 -0500

    adding datastore support - users_model.get_or_create_user changed

--- imhere/models/users_model.py
+++ imhere/models/users_model.py
@@ -1,39 +1,37 @@
+from flask import current_app
 from model import Model
 from google.cloud import datastore
 
 class Users(Model):
 
-    def get_client():
-        return datastore.Client(current_app.config['PROJECT_ID'])
+    def get_client(self): # TODO fix the config file
+        #return datastore.Client(current_app.config['PROJECT_ID'])
+        return datastore.Client('cu-ase')
 
     def get_or_create_user(self, user):
-        try:
-            # insert = """
-            # INSERT INTO users (name, family_name, email)
-            # VALUES ('{0}', '{1}', '{2}')
-            # """.format(user['given_name'], user['family_name'], user['email'])
-            #
-            # self.db.execute(insert)
-            ds = get_client()
-            key = ds.key('user')
-            entity = datastore.Entity(
-                key=key)
-            entity.update(user)
-            ds.put(entity)
-        except:  # TODO
-            pass
-
-        # query = """
-        # SELECT uid FROM users WHERE email = '{0}'
-        # """.format(user['email'])
+        ds = self.get_client()
         query = ds.query(kind='user')
         query.add_filter('email', '=', user['email'])
 
-        # result = self.db.execute(query)
         result = list(query.fetch())
-        return self.deproxy(result)[0]['uid']
+        if result:
+            print result
+        else:
+            try:
+                key = ds.key('user')
+                entity = datastore.Entity(
+                    key=key)
+                entity.update(user)
+                ds.put(entity)
+                # key2 = ds.key('user', int('5715999101812736'))
+                # ds.delete(key2)
+            except:  # TODO
+                pass
+        result = list(query.fetch())
+        return self.deproxy(result)[0]['id']
 
-    def is_valid_uni(self, uni):
+
+    def is_valid_uni(self, uni): # TODO convert to datastore
         uni = self.escape_string(uni)
         query = "select sid from students where uni = '%s'" % uni
         result = self.db.execute(query)
