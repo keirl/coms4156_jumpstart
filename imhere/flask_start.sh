3ff3295fbe63330a491d4b3f5ae06e1839bc8575
Your Name <lauritzen@gmail.com>
2017-05-05 20:47:43 -0400

    Reorganized files

--- imhere/flask_start.sh
+++
@@ -1,3 +0,0 @@
-export DATASTORE_EMULATOR_HOST=localhost:8081
-export FLASK_APP=run.py
-flask run
--- imhere/models/model.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import datastore
-from flask import current_app
-
-class Model:
-
-    def get_client(self): # TODO fix the config file
-        #return datastore.Client(current_app.config['PROJECT_ID'])
-        return datastore.Client('cu-ase')
